5dc52507356c65d4367244cab36afd373c66d8e1
KKDR1998 <77356024+KKDR1998@users.noreply.github.com>
2021-03-28 20:44:44 -1000

    Initial Commit

---
+++ ProjectTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectTwo
+//
+//  Created by User on 3/28/21.
+//  Copyright © 2021 Kalahiki Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
